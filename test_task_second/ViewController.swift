ae3ee02f4faf5f3696097c357e68a85c43db80d4
Kirill Khomicevich <khomicevichkirill@icloud.com>
2023-06-12 20:31:27 +0300

    Initial Commit

---
+++ test_task_second/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_task_second
+//
+//  Created by Kirill Khomytsevych on 12.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
